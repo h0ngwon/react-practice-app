ec7c8e704d9c232e935dd891a0e2a85a4daae071
h0ngwon <w___on@naver.com>
2023-01-27 14:55:02 +0900

    Feat : 추가한 유저 리스트 보여주는 기능

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 function App() {
-  return (
-    <div>
-      <AddUser/>
-    </div>
-  );
+	const [usersList, setUsersList] = useState([]);
+
+	const addUserHandler = (name, age) => {
+		setUsersList((prevList) => {
+			return [...prevList, { name: name, age: age }];
+		});
+	};
+
+	return (
+		<div>
+			<AddUser onAddUser={addUserHandler} />
+			<UserList users={usersList} />
+		</div>
+	);
 }
 
 export default App;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Card from '../UI/Card';
+import styles from './AddUser.module.css';
+import Button from '../UI/Button';
 
 const AddUser = (props) => {
-    const addUserHandler = (event) => {
-        event.preventDefault();
+	const [userName, setUserName] = useState('');
+	const [userAge, setUserAge] = useState('');
 
-    }
+	const addUserHandler = (event) => {
+		event.preventDefault();
+		
+        if(userName.trim().length === 0 || userAge.trim().length === 0) {
+            return;
+        }
+
+        if(+userAge < 1) {
+            return;
+        }
+        props.onAddUser(userName, userAge);
+		setUserName('');
+		setUserAge('');
+	};
+
+	const userNameHandler = (event) => {
+		setUserName(event.target.value);
+	};
+
+	const userAgeHandler = (event) => {
+		setUserAge(event.target.value);
+	};
 	return (
-		<div>
+		<Card className={styles.input}>
 			<form onSubmit={addUserHandler}>
 				<label htmlFor='username'>이름</label>
-				<input id='username' type='text' />
-                <label htmlFor='age'>나이</label>
-				<input id='age' type='number' />
-                <button type='submit'>사용자 추가</button>
+				<input
+					id='username'
+					value={userName}
+					type='text'
+					onChange={userNameHandler}
+				/>
+
+				<label htmlFor='age'>나이</label>
+				<input
+					id='age'
+					value={userAge}
+					type='number'
+					onChange={userAgeHandler}
+				/>
+
+				<Button type='submit'>사용자 추가</Button>
 			</form>
-		</div>
+		</Card>
 	);
 };
 
