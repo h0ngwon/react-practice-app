3a90466332c40f19f3bea9c80d21507b1cc07f4b
h0ngwon <w___on@naver.com>
2023-01-30 18:52:02 +0900

    Refactor : 리팩토링

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Card from '../UI/Card';
 import styles from './AddUser.module.css';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
-	const [userName, setUserName] = useState('');
-	const [userAge, setUserAge] = useState('');
+	const nameInputRef = useRef();
+	const ageInputRef = useRef();
 	const [error, setError] = useState();
 
 	const addUserHandler = (event) => {
 		event.preventDefault();
 
-		if (userName.trim().length === 0 || userAge.trim().length === 0) {
+		const enteredName = nameInputRef.current.value;
+		const enteredAge = ageInputRef.current.value;
+
+		if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
 			setError({
 				title: '잘못된 값을 입력하셨습니다.',
 				message: '이름과 나이를 정확하게 입력해주세요.',
@@ -20,29 +23,21 @@ const AddUser = (props) => {
 			return;
 		}
 
-		if (+userAge < 1) {
+		if (+enteredAge < 1) {
 			setError({
 				title: '잘못된 값을 입력하셨습니다.',
 				message: '나이를 정확하게 입력해주세요.',
 			});
 			return;
 		}
-		props.onAddUser(userName, userAge);
-		setUserName('');
-		setUserAge('');
+		props.onAddUser(enteredName, enteredAge);
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
 	};
 
-	const userNameHandler = (event) => {
-		setUserName(event.target.value);
+	const errorHandler = () => {
+		setError(null);
 	};
-
-	const userAgeHandler = (event) => {
-		setUserAge(event.target.value);
-	};
-
-    const errorHandler = () => {
-        setError(null);
-    }
 
 	return (
 		<>
@@ -50,26 +45,16 @@ const AddUser = (props) => {
 				<ErrorModal
 					title={error.title}
 					message={error.message}
-                    onConfirm={errorHandler}
+					onConfirm={errorHandler}
 				/>
 			)}
 			<Card className={styles.input}>
 				<form onSubmit={addUserHandler}>
 					<label htmlFor='username'>이름</label>
-					<input
-						id='username'
-						value={userName}
-						type='text'
-						onChange={userNameHandler}
-					/>
+					<input id='username' type='text' ref={nameInputRef} />
 
 					<label htmlFor='age'>나이</label>
-					<input
-						id='age'
-						value={userAge}
-						type='number'
-						onChange={userAgeHandler}
-					/>
+					<input id='age' type='number' ref={ageInputRef} />
 
 					<Button type='submit'>사용자 추가</Button>
 				</form>
